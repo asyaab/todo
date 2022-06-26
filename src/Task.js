6c6225b719c974d5f4a975a802812f6890432dd7
Anastasia <nastya14101997@gmail.com>
2022-06-26 11:26:54 +0300

    6

--- src/Task.js
+++ src/Task.js
@@ -11,9 +11,9 @@ export default function Task({ task, doneTask, deleteTask, editTask }) {
         <p className="task-title">{task.title}</p>
       </Checkbox>
         <div className='task_icons'>
-          if(!task.done){
-            <EditOutlined onClick={editTask} style={{ fontSize: 16, cursor:'pointer' }} />
-          }
+          {!task.done 
+          ? <EditOutlined onClick={editTask} style={{ fontSize: 16, cursor:'pointer' }} />
+          : null }
           <CloseOutlined onClick={deleteTask} style={{ fontSize: 16, cursor:'pointer' }} />
         </div>
     </div>
